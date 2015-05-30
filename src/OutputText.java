773716075e47f4d81ad721be118edfe14fb55a36
JuhCaroline <juhcaroline@outlook.com>
2015-05-29 23:23:54 -0300

    Criação das Interfaces de Entrada e Saída

---
+++ src/OutputText.java
@@ -0,0 +1,20 @@
+//package IO;
+
+//import util.Graph;
+
+public class OutputText implements Output {
+    @Override
+    public void open(String path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void write() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
